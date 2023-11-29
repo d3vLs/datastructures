c8fb9faeeb9076e9dfdbd65a8d302f833b3586f2
d3vLs <dhruv.sahani.1807@gmail.com>
2023-11-29 19:13:34 +0530

    new file:   sorting-algos/quickSort.js

---
+++ sorting-algos/quickSort.js
@@ -0,0 +1,23 @@
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+};
+
+arr = [6, 8, 10, 12, 15, 16, 18, 20, 22, 25];
+const result = quickSort(arr);
+console.log(result);
